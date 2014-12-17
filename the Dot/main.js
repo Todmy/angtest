6592c8a5a7b0af406a85075e828be4e0b23e2de3
Todmy <tolok.d@gmail.com>
2014-12-17 20:38:19 +0200

    defining a method on the scope

--- the Dot/main.js
+++ the Dot/main.js
@@ -1,6 +1,14 @@
 angular.module("app", [])
-    .factory("Data", function(){
+    .factory("Data", function () {
         return {message: "I'm data from service"}
     })
-    .controller("FirstCtrl", function first($scope, Data){$scope.data = Data})
-    .controller("SecondCtrl", function second($scope, Data){$scope.data = Data})
+    .controller("FirstCtrl", function first($scope, Data) {
+        $scope.data = Data
+    })
+    .controller("SecondCtrl", function second($scope, Data) {
+        $scope.data = Data;
+
+        $scope.revertMessage = function(mes){
+            return mes.split('').reverse().join('');
+        }
+    });
