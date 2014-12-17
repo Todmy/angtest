848f962de488665fa1d40bd072eddd5d63477629
Todmy <tolok.d@gmail.com>
2014-12-17 20:47:04 +0200

    filters

--- the Dot/main.js
+++ the Dot/main.js
@@ -2,13 +2,19 @@ angular.module("app", [])
     .factory("Data", function () {
         return {message: "I'm data from service"}
     })
+    .filter("reverse", function(){
+        return function(text){
+            return text.split("").reverse().join("");
+        }
+    })
     .controller("FirstCtrl", function first($scope, Data) {
         $scope.data = Data
     })
     .controller("SecondCtrl", function second($scope, Data) {
         $scope.data = Data;
 
-        $scope.revertMessage = function(mes){
-            return mes.split('').reverse().join('');
-        }
+        //better way to use filter for aims like this
+        //$scope.revertMessage = function(mes){
+        //    return mes.split('').reverse().join('');
+        //}
     });
