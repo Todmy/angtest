c7f4f0681ae5395b340c891dbf877be38cd4f174
Todmy <tolok.d@gmail.com>
2014-12-18 12:36:13 +0200

    added new attribute directive

--- directive/main.js
+++ directive/main.js
@@ -2,6 +2,15 @@ var myApp = angular.module("app", []);
 myApp.directive("superman", function(){
    return{
        restrict: "E",
-       template: "<h1>I'm a man!</h1>"
+       template: "<h1>I'm a strong man!</h1>"
    }
+});
+
+myApp.directive("flash", function(){
+    return{
+        restrict: "A",
+        link: function(){
+            alert("I'm a fast man!");
+        }
+    }
 });
