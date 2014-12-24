587e10534759d8d2579479a7a71c63eb1cd3bf9b
Todmy <tolok.d@gmail.com>
2014-12-24 09:43:46 +0200

    added custom filter

--- filters/script.js
+++ filters/script.js
@@ -1,3 +1,12 @@
 var myApp = angular.module('app', []);
 myApp.controller('MyController', MyController);
-function MyController () {}
+function MyController() {
+};
+function customFilter() {
+    return function (input) {
+        if (input) {
+            return input.split('').reverse().join('');
+        }
+    }
+}
+myApp.filter('customFilter', customFilter);
