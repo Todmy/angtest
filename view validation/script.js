51190b2e35d75507b98529dc90e99ca547d08e89
Todmy <tolok.d@gmail.com>
2014-12-24 12:41:41 +0200

    last version of view validation

--- view validation/script.js
+++ view validation/script.js
@@ -2,23 +2,25 @@ var myApp = angular.module('app', []);
 myApp.controller('MyController', MyController);
 function MyController($scope) {
 }
-myApp.directive('oneToTen', function () {
+myApp.directive('ensureUnique', function ($http) {
     return {
-        require: '?ngModel',
-        link: function(scope,elem, attrs, ngModel) {
-            if(!ngModel) return;
-                ngModel.$parsers.unshift(
-                    function (viewValue) {
-                        var i = parseInt (viewValue)
-                        if(i >=0 && i<10){
-                            ngModel.$setValidity('oneToTen', true);
-                            return viewValue;
-                        } else {
-                            ngModel.$setValidity('oneToTen', false)
-                            return undefined;
-                        }
+        require: 'ngModel',
+        link: function(scope, elem, attrs, ngModel) {
+            scope.$watch(attrs.ngModel, function (n) {
+                if (!n) return;
+                $http({
+                    method: 'POST',
+                    url: '/api/check/'+attrs.ensureUnique,
+                    data: {
+                        fild: attrs.ensureUnique,
+                        value: scope.ngModel
                     }
-                )
+                }).success(function(data){
+                    c.$setValidity('unique', data.isUnique);
+                }).error(function(data) {
+                    c.$setValidity('unique', false);
+                })
+            })
         }
     }
-})
+});
